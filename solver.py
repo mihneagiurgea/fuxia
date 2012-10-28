30bd2186e2b94f3a6c4414123b5c6d0d6654a9d8
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2012-10-28 06:10:23 +0200

    solver now uses picloud

--- solver.py
+++ solver.py
@@ -1,11 +1,21 @@
 # States:
 import copy
+import cloud
 
-from board import Board
+# In order to reduce overhead, only perform cloud-forks once every X solve
+# function calls, instead of once every call.
+MAX_LOCALHOST_DEPTH = 30
 
-def solve(board):
+def solve(board, depth=0):
     """Tries to solve a board instance, returning the first solution found.
-    Returns None when no solution exists."""
+    Returns None when no solution exists.
+
+    Current implementation uses picloud to parallelize the recursive calls
+    of this function. However, in order to reduce overhead, instead of
+    making each recursive call a remote procedure call, only do that
+    once every few levels of recursion. The effect of this is that each
+    job will process several levels from the recursion tree.
+    """
     # Find the empty cell with the minimum number of filling possibilities.
     mini, minj, minlen = 10, 10, 10
     for i in xrange(9):
@@ -28,18 +38,33 @@ def solve(board):
     # Fill in the selected cell with each possible digit,
     # until we find a solution.
     possibilities = board.get_possibilities(mini, minj)
+    if len(possibilities) == 0:
+        return None
+    elif len(possibilities) == 1 or depth < MAX_LOCALHOST_DEPTH:
+        return branch_out_on_local(board, mini, minj, possibilities, depth+1)
+    else:
+        return branch_out_in_cloud(board, mini, minj, possibilities, 0)
+
+def branch_out_in_cloud(board, i, j, possibilities, depth):
+    jids = []
     for digit in possibilities:
         new_board = copy.deepcopy(board)
-        new_board.fill(mini, minj, digit)
-        partial_result = solve(new_board)
+        new_board.fill(i, j, digit)
+        jid = cloud.call(solve, new_board, depth)
+        jids.append(jid)
+        # partial_result = solve(new_board)
+    for result in cloud.iresult(jids):
+        if result:
+            # TODO - kill all jobs
+            return result
+    return None
+
+def branch_out_on_local(board, i, j, possibilities, depth):
+    for digit in possibilities:
+        new_board = copy.deepcopy(board)
+        new_board.fill(i, j, digit)
+        partial_result = solve(new_board, depth)
         if partial_result:
            return partial_result
 
     return None
-
-if __name__ == '__main__':
-    board = Board.from_file('test/fixtures/insane_board.txt')
-    print 'Read board:\n%r' % board
-
-    solution = solve(board)
-    print 'Solution:\n%r' % solution
