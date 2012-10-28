c7c285943fabb87132382b3be0b833744aab8483
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2012-10-28 03:59:40 +0300

    Updated DiggingStrategy + tests

--- digging_strategy.py
+++ digging_strategy.py
@@ -1,14 +1,49 @@
 import random
 
 class DiggingStrategy(object):
+    """TODO - write me"""
 
     def __init__(self, difficulty):
-        self.generate_randomized_cells()
-        self.limit = 10
+        if not isinstance(difficulty, int):
+            raise ValueError('invalid difficulty argument: expected int')
+        if difficulty in (1, 2):
+            self.generate_randomized_cells()
+        elif difficulty == 3:
+            self.generate_jumping_once_cell()
+        elif difficulty == 4:
+            self.generate_wandering_along_s()
+        elif difficulty == 5:
+            self.generate_ordered_cells()
+        else:
+            raise ValueError('invalid difficulty level: %d' % difficulty)
 
-    def generate_randomized_cells(self):
+        self.limit = 50
+
+    def generate_ordered_cells(self):
         self.cells = []
         for i in xrange(9):
             for j in xrange(9):
                 self.cells.append((i,j))
+
+    def generate_randomized_cells(self):
+        self.generate_ordered_cells()
         random.shuffle(self.cells)
+
+    def generate_wandering_along_s(self):
+        self.cells = []
+        for i in xrange(9):
+            for j in xrange(9):
+                if i % 2 == 0:
+                    cell = (i, j)
+                else:
+                    cell = (i, 8-j)
+                self.cells.append(cell)
+
+    def generate_jumping_once_cell(self):
+        self.generate_wandering_along_s()
+        temp = []
+        for i in xrange(0, len(self.cells), 2):
+            temp.append(self.cells[i])
+        for i in xrange(1, len(self.cells), 2):
+            temp.append(self.cells[i])
+        self.cells = temp
--- test/test_digging_strategy.py
+++ test/test_digging_strategy.py
@@ -5,15 +5,16 @@ from digging_strategy import DiggingStrategy
 class TestDiggingStrategy(unittest.TestCase):
 
     def test_generate_randomized_cells(self):
-        test = DiggingStrategy(1)
-        # Test cells in strategy:
-        # have coordinates of form: (x,y)
-        # with values x, y between 0 and 9
-        for cell in test.cells:
-            self.assertTrue(isinstance(cell, tuple))
-            self.assertTrue(len(cell) == 2)
-            self.assertIn(cell[0], range(0,9))
-            self.assertIn(cell[1], range(0,9))
-        # Test we have 81 different cells.
-        self.assertTrue(len(test.cells) == 81)
-        self.assertTrue( len(set(test.cells)) == len(test.cells) )
+        for difficulty in (1, 2, 3, 4, 5):
+            test = DiggingStrategy(difficulty)
+            # Test cells in strategy:
+            # have coordinates of form: (x,y)
+            # with values x, y between 0 and 9
+            for cell in test.cells:
+                self.assertTrue(isinstance(cell, tuple))
+                self.assertTrue(len(cell) == 2)
+                self.assertIn(cell[0], range(0,9))
+                self.assertIn(cell[1], range(0,9))
+            # Test we have 81 different cells.
+            self.assertTrue(len(test.cells) == 81)
+            self.assertTrue( len(set(test.cells)) == len(test.cells) )
