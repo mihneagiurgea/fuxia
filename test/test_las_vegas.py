0d3cb8db0aa28a963dc139aadc59af8c7fff8df1
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2012-10-28 03:34:55 +0200

    Minor test_las_vegas change

--- test/test_las_vegas.py
+++ test/test_las_vegas.py
@@ -1,13 +1,13 @@
 import unittest2 as unittest
 
-from las_vegas import las_vegas
+from las_vegas import generate_terminal_pattern
 
 class TestLasVegas(unittest.TestCase):
     """Testing the Las Vegas algorithm, the returned board must be a valid
     final configuration."""
 
     def test_las_vegas(self):
-        board = las_vegas()
+        board = generate_terminal_pattern()
 
         # Test the generated board is valid.
         self.assertIsNotNone(board)
@@ -16,4 +16,4 @@ class TestLasVegas(unittest.TestCase):
         # Test board doesn't have any empty cells.
         for i in xrange(0,9):
             for j in xrange(0,9):
-                self.assertTrue(board._board[i][j] != 0)
+                self.assertTrue(board.get(i, j) != 0)
