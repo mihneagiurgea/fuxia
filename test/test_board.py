92362b57d2329249739a29ab0ec9b70bf11e8aaf
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2012-10-28 06:30:59 +0200

    Modified tests to run on cloud simulator

--- test/test_board.py
+++ test/test_board.py
@@ -1,4 +1,4 @@
-import unittest2 as unittest
+import unittest
 
 from board import Board
 
--- test/test_digger.py
+++ test/test_digger.py
@@ -1,9 +1,13 @@
 import unittest
+import cloud
 
 from digger import Digger
 from digging_strategy import DiggingStrategy
 from las_vegas import generate_terminal_pattern
 from solver import solve
+
+# Initiate the picloud simulator (localhost only) to speed up tests.
+cloud.start_simulator()
 
 class TestDigger(unittest.TestCase):
 
